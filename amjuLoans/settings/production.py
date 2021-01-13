1c5ecfd34b7f9e2cbf0c851387fbc197f8cc3c78
mathemartins1 <mathegeniuse@gmail.com>
2021-01-13 14:55:37 -0800

    ok

--- amjuLoans/settings/production.py
+++ amjuLoans/settings/production.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '(=vw2)fyh)4uit)gty2z7g-icg$3a^zc3w-eb)v+)@tncxd%))'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 BASE_URL = 'https://amju.herokuapp.com'
 
