73ac3972116b632b61441ead1282dd54b7b2eae0
mathemartins1 <mathegeniuse@gmail.com>
2021-02-28 15:06:23 -0800

    ok

--- borrowers/api/views.py
+++ borrowers/api/views.py
@@ -5,7 +5,42 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from accounts.models import Profile
-from borrowers.models import Borrower
+from borrowers.models import Borrower, BorrowerBankAccount
+
+
+class BorrowerAccountDetails(RetrieveAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_class = JSONWebTokenAuthentication
+
+    def get(self, request):
+        try:
+            user_profile_obj = Profile.objects.get(user=request.user)
+            borrower_obj = Borrower.objects.get(user=user_profile_obj)
+            borrower_amju_account = BorrowerBankAccount.objects.get(borrower=borrower_obj)
+            status_code = status.HTTP_200_OK
+            response = {
+                'success': 'true',
+                'status_code': status_code,
+                'message': 'Borrower Account fetched successfully',
+                'data': [{
+                    'account_number': borrower_amju_account.account_no,
+                    'account_type': borrower_amju_account.account_type,
+                    'balance': borrower_amju_account.balance,
+                    'active': borrower_amju_account.active,
+                    'timestamp': borrower_amju_account.timestamp,
+                    'updated': borrower_amju_account.updated,
+                }]
+            }
+
+        except Exception as e:
+            status_code = status.HTTP_400_BAD_REQUEST
+            response = {
+                'success': 'false',
+                'status code': status.HTTP_400_BAD_REQUEST,
+                'message': 'Borrower Account does not exists',
+                'error': str(e)
+            }
+        return Response(response, status=status_code)
 
 
 class BorrowerDetailView(RetrieveAPIView):
