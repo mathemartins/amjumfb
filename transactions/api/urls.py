0c4dd3e98360097cc6db0c1bfaa53119b0030328
mathemartins1 <mathegeniuse@gmail.com>
2021-03-03 21:23:48 -0800

    transaction update

--- transactions/api/urls.py
+++ transactions/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from .views import TransactionListAPIView, FundingTransactionView
+from .views import TransactionListAPIView, FundingTransactionView, WithdrawalTransactionView
 
 urlpatterns = [
     path('', TransactionListAPIView.as_view(), name='loan-request-list'),
     path('fund-my-account/', FundingTransactionView.as_view(), name='fund-account'),
-    path('withdraw-my-fund/', FundingTransactionView.as_view(), name='withdraw-fund'),
+    path('withdraw-my-fund/', WithdrawalTransactionView.as_view(), name='withdraw-fund'),
     # path('<slug:slug>/details/', LoanRequestDetailAPIView.as_view(), name='loan-request-detail'),
 ]
